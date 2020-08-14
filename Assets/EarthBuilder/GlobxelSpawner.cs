27b390e4d3e50b897087c9df22728564accee450
Bryan P. Arant <brynarnt@protonmail.com>
2020-08-14 13:32:54 -0700

    First commit. 2000 spheres.

---
+++ Assets/EarthBuilder/GlobxelSpawner.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Unity.Rendering;
+using Unity.Physics;
+using Unity.Mathematics;
+
+public class GlobxelSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _prefab;
+
+    [SerializeField]
+    Mesh _defaultMesh;
+
+    [SerializeField]
+    UnityEngine.Material _defaultMaterial;
+
+    float _spawnPeriod = 1f / 500f;
+    float _lastSpawnTime = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void SpawnGlobxelConvert(Vector3 position)
+    {
+        Instantiate(_prefab, position, Quaternion.identity);
+    }
+
+    // TODO: Fails with AABB errors. I dont know what I am doing in most of these lines though.
+    void SpawnGlobxelNew(Vector3 position)
+    {
+        // TODO : Create entity here with data.
+        Debug.Log($"TODO: Spawn at {position}");
+        var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var baseGlobxelArchetype = em.CreateArchetype(
+            typeof(Translation),
+            typeof(Rotation),
+            typeof(RenderMesh),
+            typeof(RenderBounds),
+            typeof(LocalToWorld),
+            typeof(PhysicsMass),
+            typeof(PhysicsVelocity),
+            typeof(PhysicsCollider)
+            );
+
+        var newEntity = em.CreateEntity(baseGlobxelArchetype);
+
+        // TODO: what is shared component data vs component data?
+
+        em.AddComponentData(newEntity, new Translation
+        {
+            Value = position
+        });
+
+        em.AddSharedComponentData(newEntity, new RenderMesh
+        {
+            mesh = _defaultMesh,
+            material = _defaultMaterial
+        });
+
+        em.AddComponentData(newEntity, new RenderBounds
+        {
+            Value = new AABB
+            {
+                Center = float3.zero,
+                Extents = new float3(1f, 1f, 1f)
+            }
+        });
+
+        em.AddComponentData(newEntity, new PhysicsVelocity
+        {
+            Angular = new float3(0f, 0f, 0f),
+            Linear = new float3(1f, 0f, 0f)
+        });
+
+
+        if (em.HasComponent<PhysicsMass>(newEntity))
+        {
+            var mass = em.GetComponentData<PhysicsMass>(newEntity);
+            mass.InverseMass = 1f;
+            em.SetComponentData<PhysicsMass>(newEntity, mass);
+        }
+
+
+        em.AddComponentData(newEntity, new PhysicsCollider
+        {
+            Value = Unity.Physics.BoxCollider.Create(new BoxGeometry()
+            {
+                Size = new float3(1, 1, 1),
+                Center = new float3(0.5f, 0.5f, 0.5f),
+                Orientation = quaternion.identity,
+                BevelRadius = 0
+            })
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            if(Time.time - _lastSpawnTime >= _spawnPeriod)
+            {
+                SpawnGlobxelConvert(Camera.main.transform.position + 10f*Camera.main.transform.forward);
+                _lastSpawnTime = Time.time;
+            }
+        }
+    }
+}
---
+++ Assets/EarthBuilder/Systems/ForceFieldSystem.cs
@@ -0,0 +1,72 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Physics;
+using Unity.Physics.Extensions;
+using System.Numerics;
+
+public class ForceFieldSystem : SystemBase
+{
+    NativeArray<float3> positions;
+
+    protected override void OnCreate()
+    {
+        positions = new NativeArray<float3>(5000, Allocator.Persistent);
+        base.OnCreate();
+    }
+
+    protected override void OnDestroy()
+    {
+        positions.Dispose();
+        base.OnDestroy();
+    }
+
+    private EntityQuery query;
+    protected override void OnUpdate()
+    {
+        int entitiesInQuery = query.CalculateEntityCount();
+        float dt = Time.DeltaTime;
+        var positions = this.positions;
+
+        Entities
+            .ForEach((int entityInQueryIndex, in Translation translation) =>
+        {
+            positions[entityInQueryIndex] = translation.Value;
+        })
+        .WithStoreEntityQueryInField(ref query)
+        .ScheduleParallel();
+
+        Entities
+            .ForEach((
+                ref PhysicsVelocity velocity,
+                in PhysicsMass mass,
+                in Translation translation,
+                in Rotation rotation) =>
+        {
+            float3 resultant = float3.zero;
+            float3 diff = float3.zero;
+
+            for (int i = 0; i < entitiesInQuery; i++)
+            {
+                diff = positions[i] - translation.Value;
+
+                if (math.lengthsq(diff) < 1f)
+                {
+                    continue;
+                }
+
+                // Newton's law of gravity
+                resultant += (1f / math.pow(math.pow(diff.x, 2) + math.pow(diff.y, 2) + math.pow(diff.z, 2), 1.5f)) * math.normalize(diff);
+            }
+
+            // newtons second
+            velocity.ApplyImpulse(mass, translation, rotation, resultant, translation.Value);
+        })
+        .ScheduleParallel();
+
+    }
+}
