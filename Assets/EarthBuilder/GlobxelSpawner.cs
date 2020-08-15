39f7f863982774a51d634d6f85c6ef70c8a782f1
Bryan P. Arant <brynarnt@protonmail.com>
2020-08-14 23:13:56 -0700

    added a liquid ion type thing.

--- Assets/EarthBuilder/GlobxelSpawner.cs
+++ Assets/EarthBuilder/GlobxelSpawner.cs
@@ -8,18 +8,23 @@ using Unity.Rendering;
 using Unity.Physics;
 using Unity.Mathematics;
 
+
 public class GlobxelSpawner : MonoBehaviour
 {
     [SerializeField]
-    GameObject _prefab;
+    GameObject _earthPrefab;
 
     [SerializeField]
-    Mesh _defaultMesh;
+    GameObject _waterPrefab;
 
-    [SerializeField]
-    UnityEngine.Material _defaultMaterial;
+    // TODO: These are part of the new SpawnGlobxelNew methods. I will update eventually.
+    //[SerializeField]
+    //Mesh _defaultMesh;
 
-    float _spawnPeriod = 1f / 500f;
+    //[SerializeField]
+    //UnityEngine.Material _defaultMaterial;
+
+    float _spawnPeriod = 1f / 1000f;
     float _lastSpawnTime = 0f;
 
     // Start is called before the first frame update
@@ -27,78 +32,81 @@ public class GlobxelSpawner : MonoBehaviour
     {
         
     }
-    void SpawnGlobxelConvert(Vector3 position)
+
+    // This requires that the prefabs have an entity converter on them. 
+    void SpawnGlobxelConvert(GameObject prefab, Vector3 position)
     {
-        Instantiate(_prefab, position, Quaternion.identity);
+        Instantiate(prefab, position, Quaternion.identity);
     }
 
     // TODO: Fails with AABB errors. I dont know what I am doing in most of these lines though.
-    void SpawnGlobxelNew(Vector3 position)
-    {
-        // TODO : Create entity here with data.
-        Debug.Log($"TODO: Spawn at {position}");
-        var em = World.DefaultGameObjectInjectionWorld.EntityManager;
-        var baseGlobxelArchetype = em.CreateArchetype(
-            typeof(Translation),
-            typeof(Rotation),
-            typeof(RenderMesh),
-            typeof(RenderBounds),
-            typeof(LocalToWorld),
-            typeof(PhysicsMass),
-            typeof(PhysicsVelocity),
-            typeof(PhysicsCollider)
-            );
+    // Consult Unity documentation for how to create entities to get this right - it will matter later.
+    //void SpawnGlobxelNew(Vector3 position)
+    //{
+    //    // TODO : Create entity here with data.
+    //    Debug.Log($"TODO: Spawn at {position}");
+    //    var em = World.DefaultGameObjectInjectionWorld.EntityManager;
+    //    var baseGlobxelArchetype = em.CreateArchetype(
+    //        typeof(Translation),
+    //        typeof(Rotation),
+    //        typeof(RenderMesh),
+    //        typeof(RenderBounds),
+    //        typeof(LocalToWorld),
+    //        typeof(PhysicsMass),
+    //        typeof(PhysicsVelocity),
+    //        typeof(PhysicsCollider)
+    //        );
 
-        var newEntity = em.CreateEntity(baseGlobxelArchetype);
+    //    var newEntity = em.CreateEntity(baseGlobxelArchetype);
 
-        // TODO: what is shared component data vs component data?
+    //    // TODO: what is shared component data vs component data?
 
-        em.AddComponentData(newEntity, new Translation
-        {
-            Value = position
-        });
+    //    em.AddComponentData(newEntity, new Translation
+    //    {
+    //        Value = position
+    //    });
 
-        em.AddSharedComponentData(newEntity, new RenderMesh
-        {
-            mesh = _defaultMesh,
-            material = _defaultMaterial
-        });
+    //    em.AddSharedComponentData(newEntity, new RenderMesh
+    //    {
+    //        mesh = _defaultMesh,
+    //        material = _defaultMaterial
+    //    });
 
-        em.AddComponentData(newEntity, new RenderBounds
-        {
-            Value = new AABB
-            {
-                Center = float3.zero,
-                Extents = new float3(1f, 1f, 1f)
-            }
-        });
+    //    em.AddComponentData(newEntity, new RenderBounds
+    //    {
+    //        Value = new AABB
+    //        {
+    //            Center = float3.zero,
+    //            Extents = new float3(1f, 1f, 1f)
+    //        }
+    //    });
 
-        em.AddComponentData(newEntity, new PhysicsVelocity
-        {
-            Angular = new float3(0f, 0f, 0f),
-            Linear = new float3(1f, 0f, 0f)
-        });
-
-
-        if (em.HasComponent<PhysicsMass>(newEntity))
-        {
-            var mass = em.GetComponentData<PhysicsMass>(newEntity);
-            mass.InverseMass = 1f;
-            em.SetComponentData<PhysicsMass>(newEntity, mass);
-        }
+    //    em.AddComponentData(newEntity, new PhysicsVelocity
+    //    {
+    //        Angular = new float3(0f, 0f, 0f),
+    //        Linear = new float3(1f, 0f, 0f)
+    //    });
 
 
-        em.AddComponentData(newEntity, new PhysicsCollider
-        {
-            Value = Unity.Physics.BoxCollider.Create(new BoxGeometry()
-            {
-                Size = new float3(1, 1, 1),
-                Center = new float3(0.5f, 0.5f, 0.5f),
-                Orientation = quaternion.identity,
-                BevelRadius = 0
-            })
-        });
-    }
+    //    if (em.HasComponent<PhysicsMass>(newEntity))
+    //    {
+    //        var mass = em.GetComponentData<PhysicsMass>(newEntity);
+    //        mass.InverseMass = 1f;
+    //        em.SetComponentData<PhysicsMass>(newEntity, mass);
+    //    }
+
+
+    //    em.AddComponentData(newEntity, new PhysicsCollider
+    //    {
+    //        Value = Unity.Physics.BoxCollider.Create(new BoxGeometry()
+    //        {
+    //            Size = new float3(1, 1, 1),
+    //            Center = new float3(0.5f, 0.5f, 0.5f),
+    //            Orientation = quaternion.identity,
+    //            BevelRadius = 0
+    //        })
+    //    });
+    //}
 
     // Update is called once per frame
     void Update()
@@ -107,7 +115,15 @@ public class GlobxelSpawner : MonoBehaviour
         {
             if(Time.time - _lastSpawnTime >= _spawnPeriod)
             {
-                SpawnGlobxelConvert(Camera.main.transform.position + 10f*Camera.main.transform.forward);
+                SpawnGlobxelConvert(_earthPrefab, Camera.main.transform.position + 10f*Camera.main.transform.forward);
+                _lastSpawnTime = Time.time;
+            }
+        }
+        else if(Input.GetKey(KeyCode.Space))
+        {
+            if (Time.time - _lastSpawnTime >= _spawnPeriod)
+            {
+                SpawnGlobxelConvert(_waterPrefab, Camera.main.transform.position + 10f * Camera.main.transform.forward);
                 _lastSpawnTime = Time.time;
             }
         }
---
+++ Assets/EarthBuilder/Systems/GlobPointSystem.cs
@@ -0,0 +1,108 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Physics;
+using Unity.Physics.Extensions;
+using System.Numerics;
+using System;
+using UnityEngine;
+
+// TODO: How do I order my systems?
+// TODO: See Unity.Entities.UpdateAfterAttribute and UpdateBeforeAttribute
+public class GlobPointSystem : SystemBase
+{
+    
+    [Flags]
+    public enum GlobKind: byte
+    {
+        MASS_PARTICLE = 1,
+        ION = 2
+    }
+    public const float GRAVITATIONAL_CONSTANT = 0.1f;//0.0075f;
+    public const float GLOBINESS_CONSTANT = 1f;
+
+    struct GlobPoint
+    {
+        public float Globiness;
+        public float Mass;
+        public float3 Position;
+    }
+
+    NativeArray<GlobPoint> _globPoints;
+
+    protected override void OnCreate()
+    {
+        _globPoints = new NativeArray<GlobPoint>(5000, Allocator.Persistent);
+        base.OnCreate();
+    }
+
+    protected override void OnDestroy()
+    {
+        _globPoints.Dispose();
+        base.OnDestroy();
+    }
+
+    private EntityQuery query;
+    protected override void OnUpdate()
+    {
+        int entitiesInQuery = query.CalculateEntityCount();
+        float dt = Time.DeltaTime;
+        var globPoints = this._globPoints;
+
+        Entities
+            .ForEach((int entityInQueryIndex, in Translation translation, in PhysicsMass mass, in PhysicsCustomTags tag) =>
+        {
+            if ((tag.Value & (byte)GlobKind.ION) != 0)
+            {
+                globPoints[entityInQueryIndex] = new GlobPoint { Mass = 1f/mass.InverseMass, Globiness = 0.01f, Position = translation.Value };
+            }
+            else
+            {
+                globPoints[entityInQueryIndex] = new GlobPoint { Mass = 1f/mass.InverseMass, Globiness = 0f, Position = translation.Value };
+            }
+
+        })
+        .WithStoreEntityQueryInField(ref query)
+        .ScheduleParallel();
+
+        Entities
+            .ForEach((
+                ref PhysicsVelocity velocity,
+                in PhysicsMass mass,
+                in Translation translation,
+                in Rotation rotation,
+                in PhysicsCustomTags tag) =>
+        {
+            float3 resultant = float3.zero;
+            float3 diff = float3.zero;
+            
+            for (int i = 0; i < entitiesInQuery; i++)
+            {
+                diff = globPoints[i].Position - translation.Value;
+
+                if (math.lengthsq(diff) < 1f)
+                {
+                    continue;
+                }
+                
+                // Newton's law of gravity
+                resultant += GRAVITATIONAL_CONSTANT * (globPoints[i].Mass / math.pow(math.pow(diff.x, 2) + math.pow(diff.y, 2) + math.pow(diff.z, 2), 1.5f)) * math.normalize(diff);
+
+                if ((tag.Value & (byte)GlobKind.ION) != 0)
+                {
+                    resultant += GLOBINESS_CONSTANT * (globPoints[i].Globiness / math.pow(math.pow(diff.x, 2) + math.pow(diff.y, 2) + math.pow(diff.z, 2), 1.5f)) * math.normalize(diff);
+                }                
+
+            }
+
+            // newtons second
+            velocity.ApplyImpulse(mass, translation, rotation, resultant, translation.Value);
+        })
+        .ScheduleParallel();
+
+    }
+}
